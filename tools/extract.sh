74c4e562e702e0140872dfd42d9d6c8f46ee97d8
CarsonFoster <carsonwfoster@gmail.com>
2023-10-28 15:50:00 -0500

    Add comments to scripts
    I'm aware that bash is a "crufty" (to use the language of Corky)
    language, so I've included comments explaining things that probably
    aren't obvious if you're either unfamiliar with bash or Unix utilities.

--- tools/extract.sh
+++ tools/extract.sh
@@ -14,24 +14,28 @@ usage() {
     printf 'INPUT\t\t\t\tThe input PDF file\n'
 }
 
+# Check for help flags or incorrect number of arguments
 if [[ $# -ne 1 || "$1" == "-h" || "$1" == "--help" ]]; then
     usage
     exit 1
 fi
 
-input="$1"
-root=$(pwd)
+input="$1" # input is the first command-line argument
+root=$(pwd) # root is the current directory
 
 printf 'Splitting "%s" into pages...' "$input"
 mkdir pages
+# Splits the input PDF into many PDFs, each that is one page.
 pdftk "$input" burst output pages/%02d.pdf
 printf '\rSplitting "%s" into pages...Done.\n' "$input"
 
 mkdir images
-i=1
+i=1 # page number
 for pagePath in pages/*.pdf; do
+    # name of page -- strips pdf extension and path
+    # should be a two digit number
     pageName=$(printf "$pagePath" | sed 's?^\(.*/\)*\(.*\)\.pdf$?\2?')
-    imageDir="$root/images/$pageName"
+    imageDir="$root/images/$pageName" # directory to output images for this page
 
     mkdir "$imageDir"
     cd "$imageDir"
@@ -39,25 +43,39 @@ for pagePath in pages/*.pdf; do
     printf 'Extracting images from page %d:\n' $i
     /usr/bin/env python -m pdfly extract-images "$root/$pagePath"
 
+    # Set the array "images" to contain the jp2 files that are at least 100K in size
+    # These are the images we'll convert to png
     mapfile -t images < <(ls -l *.jp2 | awk '{if ($5 >= 100000) print $9}')
     for image in "${images[@]}"; do
+        # Replace .jp2 extension with .png
         imageOutput=$(printf "$image" | sed 's/\.jp2/.png/')
         printf 'Converting %s to %s:\n' "$image" "$imageOutput"
+        # Convert jp2 file to a png
         opj_decompress -i "$image" -o "$imageOutput"
     done
     
     rm *.jp2
+    # Set the array "remove" to contain all files that are less than 100K in size
     mapfile -t remove < <(ls -l | awk 'NR > 1{if ($5 < 100000) print $9}')
+    # Delete every file in "remove" if it's not empty.
     if [[ ${#remove[@]} -gt 0 ]]; then
         rm "${remove[@]}"
     fi
 
+    # If this file exists, it's likely a composite image consisting of multiple
+    # butterfly images. We'll split it by whitespace into the component images.
+    # Note that this heuristic is only an approximation, as some pages
+    # are layed out slightly differently.
     if [[ -e "0-Im1.png" ]]; then
         printf 'Splitting images (page %d)\n' $i
+        # Use multicrop script to split by whitespace.
         "$root/multicrop" 0-Im1.png split.png
+        # Remove the composite image
         rm 0-Im1.png
     else
+        # Images were included separately in the PDF.
         printf 'Images are already split (page %d)\n' $i
+        # Likely an image containing text and such
         rm 0-Im0.png
     fi
 
--- tools/rename.sh
+++ tools/rename.sh
@@ -8,23 +8,32 @@ usage() {
     printf 'OUTDIR\t\t\t\tThe output folder\n'
 }
 
+# Check for help flags and incorrect number of arguments
 if [[ $# -ne 2 || "$1" == "-h" || "$1" == "--help" ]]; then
     usage
     exit 1
 fi
 
-indir="$1"
-outdir="$2"
+indir="$1" # first argument is input directory
+outdir="$2" # second argument is output directory
 
 mkdir "$outdir"
 
+# Set the array "dirs" to contain the child directories of $indir.
 mapfile -t dirs < <(ls -ld "$indir"/*/ | awk '{print $9}')
 for dir in "${dirs[@]}"; do
     printf 'Traversing %s:\n' "$dir"
+    # Set the array "files" to contain the child files in $dir.
     mapfile -t files < <(ls -l "$dir"* | awk '{print $9}')
+    # Take last path component.
+    # For example, this transforms input/01/ to 01
     dirName=$(printf "$dir" | sed 's~^\([^/]*/\)*\([^/]\+\)/\?$~\2~')
     i=1
     for file in "${files[@]}"; do
+        # Need to construct the sed command separately to include variable values in it
+        # The sed command replaces the entire file path with the intended filename,
+        # in the form butterfly-01-1.png.
+        # Note that the extension of the input is maintained for the output.
         sedLine=$(printf 's?^.\+\.\([^.]\+\)$?butterfly-%s-%d.\\1?' "$dirName" $i)
         outFilename=$(printf "$file" | sed $sedLine)
         outFile="$outdir/$outFilename"
