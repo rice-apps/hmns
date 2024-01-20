d75b4cf41e0becca02270aa9cd018cbd928e7832
moyindavid16 <moyindavid16@gmail.com>
2024-01-20 15:45:25 -0600

    style: resources section

--- hmns-app/client/app/(tabs)/index.tsx
+++ hmns-app/client/app/(tabs)/index.tsx
@@ -1,50 +1,77 @@
-import { Dimensions, Image, Platform, Pressable, SafeAreaView, ScrollView, StatusBar, StyleSheet } from "react-native";
-import { Text, View } from "../../components/Themed";
-import { useState } from "react";
-import { colors } from "../../constants/appColors";
-
+import {Dimensions, Image, Platform, Pressable, SafeAreaView, ScrollView, StatusBar, StyleSheet} from "react-native";
+import {Text, View} from "../../components/Themed";
+import {useState} from "react";
+import {colors} from "../../constants/appColors";
 
 interface Challenge {
-	title: string,
-	status: boolean,
-	content: string,
+  title: string;
+  status: boolean;
+  content: string;
 }
 
+interface Resource {
+  title: string;
+  content: string;
+}
 
-const mockChallenges:Challenge[] = [
+const mockChallenges: Challenge[] = [
 	{
 		title: "Challenge 1",
 		status: false,
-		content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod"
+		content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod",
 	},
 	{
 		title: "Challenge 2",
 		status: false,
-		content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod"
+		content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod",
 	},
 ];
 
+const mockResources: Resource[] = [
+	{
+		title: "Pollinator Gardening",
+		content: "Explore the Community Science Garden and learn about pollinators.",
+	},
+	{
+		title: "Monarch Butterflies",
+		content: "Dive into Monarch Conservation, Migration Insights, and Butterfly Gardening.",
+	},
+];
 
-export default function TabOneScreen () {
-	const [challenges, ] = useState<Challenge[]>(mockChallenges);
+export default function TabOneScreen() {
+	const [challenges] = useState<Challenge[]>(mockChallenges);
+	const [resources] = useState<Resource[]>(mockResources);
 	const windowHeight = Dimensions.get("window").height;
 	return (
 		<SafeAreaView style={styles.mainContainer}>
-			<StatusBar barStyle="dark-content"/>
-			<ScrollView style={styles.container} contentContainerStyle={{gap: 30, backgroundColor: "white", minHeight: "100%", paddingVertical: 15}} className="bg-white">
+			<StatusBar barStyle="dark-content" />
+			<ScrollView
+				style={styles.container}
+				contentContainerStyle={{
+					gap: 30,
+					backgroundColor: "white",
+					minHeight: "100%",
+					maxWidth: "100%",
+					paddingVertical: 15,
+				}}
+				className="bg-white"
+			>
 				{/*Header */}
 				<View className="flex flex-row bg-transparent justify-between items-center">
 					<Text className="text-black font-bold text-2xl">Good Afternoon!</Text>
-					<Image source={require("../../assets/images/hmns-logo.png")} className="w-16 h-16"/>
+					<Image source={require("../../assets/images/hmns-logo.png")} className="w-16 h-16" />
 				</View>
 
 				{/* BOTD */}
-				<View className={`flex flex-row rounded-md bg-[${colors.botd}] px-10 py-6`} style={{height: windowHeight*0.3}}>
+				<View
+					className={`flex flex-row rounded-md bg-[${colors.botd}] px-10 py-6`}
+					style={{height: windowHeight * 0.3}}
+				>
 					{/* Left Side */}
 					<View className="bg-transparent flex w-3/5 justify-around">
 						<Text className="font-bold text-black text-xl">Red Peacock</Text>
 						<Text className="text-black">
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod adipiscing elit, sed do eiusmod
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod adipiscing elit, sed do eiusmod
 						</Text>
 						<Pressable className="p-2 px-3 rounded-full self-start" style={{backgroundColor: colors.learnmore}}>
 							<Text className="text-xs">Learn more</Text>
@@ -67,7 +94,7 @@ export default function TabOneScreen () {
 							<Text className="text-black">View all</Text>
 						</Pressable>
 					</View>
-					
+
 					{challenges.map(challenge => (
 						<ChallengeItem key={challenge.title} challenge={challenge} />
 					))}
@@ -75,70 +102,66 @@ export default function TabOneScreen () {
 
 				{/* Resources */}
 				<View className="flex bg-transparent" style={{gap: 20}}>
-					{/* Challenges Header */}
+					{/* Resources Header */}
 					<View className="flex flex-row justify-between bg-transparent">
-						<Text className="text-black text-xl font-bold">Challenges</Text>
+						<Text className="text-black text-xl font-bold">Resources</Text>
 						<Pressable className="flex justify-around items-center">
 							<Text className="text-black">View all</Text>
 						</Pressable>
 					</View>
-					
-					{challenges.map(challenge => (
-						<ChallengeItem key={challenge.title} challenge={challenge} />
+
+					{resources.map(resource => (
+						<ResourceItem key={resource.title} resource={resource} />
 					))}
 				</View>
-
 			</ScrollView>
 		</SafeAreaView>
-		
 	);
 }
 
 const ChallengeItem = ({challenge}: {challenge: Challenge}) => {
 	return (
 		<View className="flex flex-row rounded-3xl p-4 border items-center justify-between bg-transparent">
-			<View style={{backgroundColor: "transparent", borderRadius: 9999, width: 30, height: 30, borderColor: "black", borderWidth: 2}} />
+			<View
+				style={{
+					backgroundColor: "transparent",
+					borderRadius: 9999,
+					width: 30,
+					height: 30,
+					borderColor: "black",
+					borderWidth: 2,
+				}}
+			/>
 			<View className="bg-transparent ml-2">
 				<Text className="text-black flex-wrap font-bold text-base">{challenge.title}</Text>
 				<Text className="text-black flex-wrap">{challenge.content}</Text>
-			</View>			
+			</View>
 		</View>
 	);
 };
-
+const ResourceItem = ({resource}: {resource: Resource}) => {
+	return (
+		<View className="flex flex-row rounded-3xl p-4 border items-start justify-between bg-transparent">
+			<View className="bg-black w-1/3 h-28 rounded-lg flex justify-around items-center">
+				<Text className="text-white">Photo</Text>
+			</View>
+			<View className="bg-transparent ml-2" style={{maxWidth: "64%"}}>
+				<Text className="text-black flex-wrap font-bold text-base">{resource.title}</Text>
+				<Text className="text-black flex-wrap">{resource.content}</Text>
+			</View>
+		</View>
+	);
+};
 
 const styles = StyleSheet.create({
 	mainContainer: {
 		display: "flex",
 		alignItems: "center",
 		backgroundColor: "white",
-		paddingTop: Platform.OS=="android"? StatusBar.currentHeight: 0
+		paddingTop: Platform.OS == "android" ? StatusBar.currentHeight : 0,
 	},
 	container: {
 		paddingHorizontal: "7.5%",
 		backgroundColor: "transparent",
-	},
-	challengesView: {
-		height: 400,
-		backgroundColor: "red",
-		width: "100%",
-		display: "flex",
-		justifyContent: "space-between"
-	},
-	challengeItemView: {
-		display: "flex",
-		flexDirection: "row",
-		alignItems: "center",
-		gap: 7,
-		padding: 15,
-		borderRadius: 20,
-		backgroundColor: colors.challenges,
-	},
-	topText: {
-		backgroundColor: "transparent",
-		color: "black"
-	},
-	imagePlaceholderText: {
-		color: "white"
 	},
 });
