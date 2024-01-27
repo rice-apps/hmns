bd5c0f1d1fa1116b502cd7363bdf8b9e74a0a645
CarsonFoster <carsonwfoster@gmail.com>
2024-01-27 14:45:44 -0600

    Move example filter to glossary page

--- hmns-app/client/app/(tabs)/glossary.tsx
+++ hmns-app/client/app/(tabs)/glossary.tsx
@@ -1,8 +1,18 @@
-import { Text } from "react-native";
+import { Button, View } from "react-native";
+import { FilterSlideUp } from '../../components/FilterSlideUp';
+import { useState } from "react";
 
 
 export default function Glossary(){
-	return (
-		<Text>Hello</Text>
-	);
+    const [filterVisible, setFilterVisible] = useState<boolean>(false);
+    return (
+        <View style={{alignItems: 'center', justifyContent: 'center', height: '100%'}}>
+            <FilterSlideUp
+                filterVisible={filterVisible}
+                onFilter={(size, color, detect) => /* do whatever on filter here */ 0}
+                onClose={() => setFilterVisible(false)}
+            />
+            <Button title="Show filter" onPress={() => setFilterVisible(true)} />
+        </View>
+    );
 }
