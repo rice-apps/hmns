42367c07bfd322d8e7ae66610ffe736cea1169c0
moyindavid16 <moyindavid16@gmail.com>
2024-02-03 14:41:58 -0600

    style: topbar

--- hmns-app/client/app/(tabs)/glossary.tsx
+++ hmns-app/client/app/(tabs)/glossary.tsx
@@ -1,12 +1,23 @@
-import {Text, View} from "react-native";
+import {Image, ScrollView, Text, TextInput, View} from "react-native";
 import SafeView from "../../components/SafeView";
+import {colors} from "../../constants/appColors";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 export default function Glossary() {
 	return (
 		<SafeView>
-			<View className="bg-[#FAF9F6]">
-				<Text>HEllo</Text>
-			</View>
+			<ScrollView className="w-full p-5">
+				{/* Top Bar */}
+				<View className="h-10 flex flex-row" style={{gap: 8}}>
+					<Image source={require("../../assets/images/filter-icon.png")} className="w-10 h-10" />
+					<View className="grow border rounded-2xl flex flex-row items-center px-3" style={{borderColor: colors.mossyOak}}>
+						<TextInput className="grow"/>
+						<FontAwesome name="search" />
+					</View>
+				</View>
+
+				<View className="grid grid-cols-2"></View>
+			</ScrollView>
 		</SafeView>
 	);
 }
