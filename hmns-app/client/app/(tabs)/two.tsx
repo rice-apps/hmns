0d39c5a90e549e0bd220d3571f2d18515db40e68
Daniel Cufino <dc58@rice.edu>
2024-02-03 13:46:00 -0600

    Revert "Expo camera (#13)"
    This reverts commit e28386692a64c6b5ba7586264238f2e96608fcc9.

--- hmns-app/client/app/(tabs)/two.tsx
+++ hmns-app/client/app/(tabs)/two.tsx
@@ -1,17 +1,31 @@
 import { StyleSheet } from "react-native";
-import CameraComponent from "../../components/CameraComponent";
-import { View } from "../../components/Themed";
 
-export default function TabTwoScreen() {
+import EditScreenInfo from "../../components/EditScreenInfo";
+import { Text, View } from "../../components/Themed";
+
+export default function TabTwoScreen () {
 	return (
 		<View style={styles.container}>
-			<CameraComponent/>
+			<Text style={styles.title}>Tab Two</Text>
+			<View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+			<EditScreenInfo path="app/(tabs)/two.tsx" />
 		</View>
 	);
 }
 
 const styles = StyleSheet.create({
 	container: {
+		alignItems: "center",
 		flex: 1,
+		justifyContent: "center"
 	},
+	separator: {
+		height: 1,
+		marginVertical: 30,
+		width: "80%"
+	},
+	title: {
+		fontSize: 20,
+		fontWeight: "bold"
+	}
 });
